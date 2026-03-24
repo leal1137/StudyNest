146da25518d7fcf90f9efcf457db771ddd119476
MnsB4646 <mans.berggren@outlook.com>
2026-03-24 14:10:31 +0100

    La till en knapp

---
+++ public/client.js
@@ -0,0 +1,7 @@
+// Grab the button from the HTML
+const button = document.getElementById('helloButton');
+
+// Add an event listener to trigger when clicked
+button.addEventListener('click', () => {
+    alert('Hello from the frontend JavaScript!');
+});
