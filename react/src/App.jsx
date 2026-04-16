e2b8bb055dc7a9879b2219cd8a56a8eefd404fe7
AndreasMacaveiu <andreas.macaveiu.4818@student.uu.se>
2026-04-16 11:53:08 +0200

    updated

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -17,15 +17,9 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* When the URL is "/", show the Home Page */}
         <Route path="/" element={<HomePage />} />
-        
-        {/* When the URL is "/login", show the Login Page */}
         <Route path="/login" element={<LoginPage />} />
-        
-        {/* When the URL is "/dashboard", show the Dashboard Page */}
-        <Route path="/dashboard" element={<DashboardPage />} />
-
+        <Route path="/createroom" element={<CreateRoomPage />} />
         <Route path="/virtual-room" element={<VirtualRoom />} />
       </Routes>
     </BrowserRouter>
