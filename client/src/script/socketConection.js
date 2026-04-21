5bfad810797dbda5cee74c5c79a40673efeb7e5b
Ed-Hawk-IT <edvard.hokby.8986@student.uu.se>
2026-04-21 12:40:55 +0200

    persistant login

--- client/src/script/socketConection.js
+++ client/src/script/socketConection.js
@@ -1,11 +1,23 @@
 import { io } from "socket.io-client";
 
 let token = undefined;
-let socket = undefined;
+export let socket = undefined;
+
+
 
 export function connect(){
-    
     token = localStorage.getItem('token');
-    console.log("Connecting to socket with token:", token);
-    socket = io("/", {path: "/socket.io", auth: {token: token}});
+    if (token && !socket) {
+        socket = io("/", {path: "/socket.io", auth: {token: token}});
+    }
+}
+
+export function logoutDisconnect() {
+    if (socket) {
+        socket.disconnect();
+        socket = undefined;
+        localStorage.removeItem('token');
+        token = undefined;
+        console.log("User Logged out, socket disconnected");
+    }
 }
