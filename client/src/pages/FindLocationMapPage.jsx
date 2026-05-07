3422fde44ff6a01a6187aa51f2be156130fd2143
AndreasMacaveiu <andreas.macaveiu.4818@student.uu.se>
2026-05-07 12:41:58 +0200

    added sidebar to findlocationmappage

--- client/src/pages/FindLocationMapPage.jsx
+++ client/src/pages/FindLocationMapPage.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import { SelectPlaceOnMap } from '../components/SelectPlaceOnMap';
 import { } from '../script/socketConection';
+import { Sidebar } from '../components/Sidebar';
 
 export default function FindLocationMapPage() {
 
 
   return (
     <div className="FindLocationMapPage">
+      <div style={{ position:'absolute', top:'0', left:'0', zIndex:9999}}>
+      <Sidebar/>
+      </div>
       <SelectPlaceOnMap/>
     </div>
   )
