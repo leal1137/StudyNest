c6475a080c2f9577d1009d4ab4db5d4d618757af
MnsB4646 <mans.berggren@outlook.com>
2026-05-07 10:49:19 +0200

    la till sidebar på join-virtual-room sidan

--- client/src/pages/JoinVirtualRoomPage.jsx
+++ client/src/pages/JoinVirtualRoomPage.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import '../CSSfiles/JoinRoomPage.css';
 import SidebarListofRooms from '../components/SidebarListofRooms';
 import { VirtualMapJoin } from '../components/VirtualMapJoin';
+import {Sidebar} from '../components/Sidebar';
 
 export default function JoinVirtualRoom() {
     const [rooms, setRooms] = useState([]);
@@ -16,6 +17,9 @@ export default function JoinVirtualRoom() {
 
     return (
         <div className="JoinVirtualRoom">
+            <div style={{ position: 'absolute', top: '0', left: '0', zIndex: 9999 }}>
+                <Sidebar />
+            </div>
             <SidebarListofRooms rooms={rooms} />
             <VirtualMapJoin rooms={rooms} />
         </div>
